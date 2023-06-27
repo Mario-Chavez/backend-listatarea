25a4c6ecdcb1e5df42bc96b97bdce2a9392fc580
Mario-Chavez <marioachavez1@gmail.com>
2023-06-27 18:12:21 -0300

    se realizo los controladores de buscarTarea y eliminarTarea

--- src/controllers/tarea.controller.js
+++ src/controllers/tarea.controller.js
@@ -1,6 +1,7 @@
 import Tarea from "../models/tareas";
 
 export const obtenerTareas = async (req, res) => {
+    console.log("entre primero");
     try {
         const tareas = await Tarea.find();
         res.status(200).json(tareas);
@@ -22,6 +23,38 @@ export const crearTareas = async (req, res) => {
         console.log(err);
         res.status(404).json({
             mensaje: "error en crear tarea desde controller",
+        });
+    }
+};
+export const obtenerTareaPorTitulo = async (req, res) => {
+    try {
+        const { title } = req.query; // Obtén el títle de la tarea de los parámetros de la query
+        const tarea = await Tarea.findOne({ title: title });
+        if (!tarea) {
+            return res.status(404).json({ mensaje: "Tarea no encontrada" });
+        }
+        res.status(200).json(tarea);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            mensaje: "Error al obtener la tarea desde el controlador",
+        });
+    }
+};
+export const eliminarTareaPorId = async (req, res) => {
+    try {
+        const { id } = req.query; // Obtén el ID de la tarea de los parámetros de la solicitud
+        const resultado = await Tarea.deleteOne({ _id: id });
+
+        if (resultado.deletedCount === 0) {
+            return res.status(404).json({ mensaje: "Tarea no encontrada" });
+        }
+
+        res.status(200).json({ mensaje: "Tarea eliminada correctamente" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            mensaje: "Error al eliminar la tarea desde el controlador",
         });
     }
 };
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { crearTareas, obtenerTareas } from "../controllers/tarea.controller";
+import {
+    crearTareas,
+    eliminarTareaPorId,
+    obtenerTareaPorTitulo,
+    obtenerTareas,
+} from "../controllers/tarea.controller";
 
 const router = Router();
 
 // metodos get(con su controller),post(con su controller)
 router.route("/tareas").get(obtenerTareas).post(crearTareas);
+
+// Ruta para obtener una tarea por título
+router.route("/tareas/find").get(obtenerTareaPorTitulo);
+
+// Ruta para eliminar una tarea por id
+router.route("/tareas/delet").delete(eliminarTareaPorId);
 
 export default router;
